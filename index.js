3f4efa19f422c6a3b167b8e42b1bcf2756872587
Karema <karemahamdy51@gmail.com>
2024-06-28 23:04:00 +0300

    add  problems

--- index.js
+++ index.js
@@ -40,4 +40,18 @@ console.log(counter())// 10
     }
 };
 
-//
+// 2635. Apply Transform Over Each Element in Array
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+// Please solve it without the built-in Array.map method.
+var map = function(arr, fn) {
+  arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
+  newArr = []
+  for(let i = 0; i < arr.length; i++) {
+      newArr.push(fn(arr[i], i))
+    }
+    console.log(newArr)
+    
+  };
+
+  map()
