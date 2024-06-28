e02095f455c12164108126ba82fa22e82882346b
Karema <karemahamdy51@gmail.com>
2024-06-28 22:44:03 +0300

    add  problems

--- index.js
+++ index.js
@@ -27,3 +27,17 @@ console.log(counter())// 10
   counter() // 11
   counter() // 12
  
+
+//2704. To Be Or Not To Be
+// Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+// toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+// notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+  var expect = function (val) {
+    return {
+        toBe: (val2) => (val === val2 ? true : (() => { throw new Error("Not Equal") })()),
+        notToBe: (val2) => (val !== val2 ? true : (() => { throw new Error("Equal") })())
+    }
+};
+
+//
