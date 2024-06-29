046116cc33e43e6069b7d7b3f28be46332fc5059
Karema <karemahamdy51@gmail.com>
2024-06-29 18:44:04 +0300

    add  problems

--- index.js
+++ index.js
@@ -123,3 +123,16 @@ var compose = function(functions) {
     }
 };
 
+// 2666. Allow One Function Call
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+
+var once = function(fn) {
+  let coun = 0
+ return function(...args){
+     coun++
+     if(coun==1) return (fn(...args))
+ }
+};
+
