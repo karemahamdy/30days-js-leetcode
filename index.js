40f39fe1612313c4e28a0aa0c0dcd45ead63a568
Karema <karemahamdy51@gmail.com>
2024-07-07 00:43:08 +0300

    chunk array

--- index.js
+++ index.js
@@ -368,3 +368,15 @@ function chunk(array, size) {
   }
   return chunkedArray
 }
+
+// 2631. Group By
+Array.prototype.groupBy = function(fn) {
+  const obj = {}
+   for(let e of this) {
+    const key = fn((e))
+    obj[key] ||= []
+     obj[key].push(e)
+   }
+   return obj
+};
+
