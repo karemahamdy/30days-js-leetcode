c594d3bf3885ab5b3f0d8fc37964c36e217823c3
Karema <karemahamdy51@gmail.com>
2024-07-10 04:26:59 +0300

    Debounce

--- index.js
+++ index.js
@@ -511,3 +511,13 @@ function memoize(fn) {
      }
  }
 }
+
+
+// 2627. Debounce
+var debounce = function(fn, t = 1000) {
+  let timer;
+  return function(...args) {
+      clearTimeout(timer);
+      timer = setTimeout(() => fn(...args), t);
+  }
+};
