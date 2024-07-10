58cc9804adf37acee5ddb90eef43148a1fb32717
Karema <karemahamdy51@gmail.com>
2024-07-10 04:04:38 +0300

    2625. Flatten Deeply Nested Array

--- index.js
+++ index.js
@@ -391,3 +391,20 @@ var timeLimit = function(fn, t) {
       ]);
   };
 };
+
+// 2625. Flatten Deeply Nested Array
+
+var flat = function (arr, n) {
+  if (n==0){
+      return arr;
+  }
+  let ans=[];
+  for (let i=0; i<arr.length; i++){
+      if(n>0 && Array.isArray(arr[i])){
+          ans.push(...flat(arr[i],n-1));
+      }else{
+          ans.push(arr[i]);
+      }
+  }
+ return ans   
+};
